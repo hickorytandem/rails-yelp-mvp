5fb3621853b5818bd0b17febc9db7d989c9225c2
Michael Carter <hickorytandem@gmail.com>
2021-07-08 19:26:31 +0900

    displayed reviews and ratings in show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurants_path(@restaurant)
   end
 
   private
 
   def find_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
