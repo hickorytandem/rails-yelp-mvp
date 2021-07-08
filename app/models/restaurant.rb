1b4bd8ec3bf42e261dc8a9a641fd55d80ac7d120
Michael Carter <hickorytandem@gmail.com>
2021-07-08 14:49:29 +0900

    working on revew new.html

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy # can call any restaurant instance e.g. @restaurant.reviews
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
